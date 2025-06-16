056d8a63c9109c8ba52d68a2638e39d2a0c6966a
ecastellanosr <ecastellanosrada@gmail.com>
2025-06-16 12:00:43 -0600

    added the player drawing

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
     dt = 0
     
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0), rect=None, special_flags=0)
+            
+        screen.fill((0,0,0), rect=None, special_flags=0)
+        player.draw(screen)
         pygame.display.flip()
+        #limit framerate to 60 FPS
         dt += clock.tick(60) / 1000
-        print(dt)
-
 
 
 if __name__ == "__main__":
